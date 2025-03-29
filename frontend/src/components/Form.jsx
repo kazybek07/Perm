101f3086feedfdf7a01c0fe9725daaceac42d0fe
Kazybek Akzholtayev <kazybekakzholtayev@MacBook-Pro-Kazybek.local>
2025-03-29 23:41:31 +0500

    second commit

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
-import '../styles/Form.css';
+import "../styles/Form.css"
+import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
     const [username, setUsername] = useState("");
@@ -10,53 +11,51 @@ function Form({ route, method }) {
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
-    const name = method === 'login' ? 'Login' : 'Register';
+    const name = method === "login" ? "Login" : "Register";
 
     const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault();
 
         try {
-
-            const res = await api.post(`/${route}`, {
-                username,
-                password,
-            });
-            if (method === 'login') {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access_token);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh_token);
-                navigate('/');
+            const res = await api.post(route, { username, password })
+            if (method === "login") {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate("/")
             } else {
-                navigate('/login');
+                navigate("/login")
             }
-
         } catch (error) {
-            alert(error);
+            alert(error)
         } finally {
-            setLoading(false);
+            setLoading(false)
         }
-    }
+    };
 
-    return <form onSubmit={handleSubmit} className="form-container">
-        <h1>{name}</h1>
-        <input
-            className="form-input"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-        />
-        <input
-            className="form-input"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-        />
-        <button type="submit" className="form-button">
-            {name}
-        </button>
-    </form>
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>{name}</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            {loading && <LoadingIndicator />}
+            <button className="form-button" type="submit">
+                {name}
+            </button>
+        </form>
+    );
 }
 
 export default Form;
